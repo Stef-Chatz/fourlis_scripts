588ce0aebc09577026a5c0fe89d5b12d71c64f9a
Stefanos Chatzakis <sxatzakis@yahoo.gr>
2019-07-17 16:16:17 +0300

    added option for target computer or active directory

--- info_retrieve.ps1
+++ info_retrieve.ps1
@@ -12,18 +12,35 @@ retrieve information regarding the operating system as well as hardware parts.
 
 
 Write-Host "Starting script!"
-$computers = Get-ADComputer -Filter {OperatingSystem -Like "*Windows 7*" -or OperatingSystem -Like "*Windows 8*" -or OperatingSystem -Like "*Windows 8.1*" -or OperatingSystem -Like "*Windows 10*"} |FT Name
-Write-Host "Computers found:" -ForegroundColor Yellow
 
-Write-Host $computers
+#prompt the admin for option.
+$Readhost = Read-Host "Type (1(single directory)/2(active directory)/3(cancel))"
 
-foreach ($computer in $computers) {
+if($ReadHost -eq "1") {
+  $CompName = Read-Host  "Enter computer name:"
+  if ($CompName -Like "FH*") {
+    #This retriece the cpu and print it
+    Write-Host "For computer:" $CompName -ForegroundColor Yellow
+    Write-Host "CPU:"
+    cpu = Get-WmiObject Win32_Processor  -computername $CompName | Select name
 
-  Write-Host "For computer:" $Computer -ForegroundColor Yellow
-  Write-Host "CPU:"
-  
-  $cpu = Get-WmiObject Win32_Processor  -computername $computer | Select name
-  
-  Write-Host $cpu
+    Write-Host $cpu
+  }
+
+}#end if
+
+elseif ($Readhost -eq "2") {
+  $computers = Get-ADComputer -Filter {OperatingSystem -Like "*Windows 7*" -or OperatingSystem -Like "*Windows 8*" -or OperatingSystem -Like "*Windows 8.1*" -or OperatingSystem -Like "*Windows 10*"} |FT Name
+  Write-Host "Computers found:" -ForegroundColor Yellow
+
+  Write-Host $computers
+
+  foreach ($computer in $computers) {
+    Write-Host "For computer:" $Computer -ForegroundColor Yellow
+    Write-Host "CPU:"
+    cpu = Get-WmiObject Win32_Processor  -computername $computer | Select name
+
+    Write-Host $cpu
 
   }#end foreach
+}#end elseif
