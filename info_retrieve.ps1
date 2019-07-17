64adb6524fa67f55ca86e1f62f6a20368e4abbf5
Stefanos Chatzakis <sxatzakis@yahoo.gr>
2019-07-17 12:20:47 +0300

    added fetch processor name

--- info_retrieve.ps1
+++ info_retrieve.ps1
@@ -12,14 +12,15 @@ retrieve information regarding the operating system as well as hardware parts.
 
 while($Repeat) {
   Write-Host "Starting script!"
-  $servers = Get-ADComputer -Filter {OperatingSystem -Like } |ForEach-Object {$_.Name}
-  Write-Host "Computers found: " -ForegroundColor Yellow
+  $servers = Get-ADComputer -Filter {OperatingSystem -Like "*Windows 7*" -or "*Windows 8*" -or "*Windows 8.1*" -or "*Windows 10*"} |ForEach-Object {$_.Name}
+  Write-Host "Computers found:" -ForegroundColor Yellow
 
   Write-Host $servers
 
   foreach ($server in $servers) {
     Invoke-Command -ComputerName $server -ScriptBlock{
-
+      Write-Host "The computer $server is using:"
+      Get_WmiObject win32_processor -Name 
     }#end Invoke-Command
   }#end foreach
 }#end while
