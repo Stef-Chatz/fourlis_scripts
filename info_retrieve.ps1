bae02471afd184885ff9bbb463d1c78d8b10ae62
Stefanos Chatzakis <sxatzakis@yahoo.gr>
2019-07-17 11:57:49 +0300

    first commit

---
+++ info_retrieve.ps1
@@ -0,0 +1,21 @@
+<#
+Author: Stefanos Chatzakis
+Version: 1.2
+Company: Fourlis Group
+Created on 04/07/2019
+#>
+
+<#
+The purpose of this script is to access the active directory computers and
+retrieve information regarding the operating system as well as hardware parts.
+#>
+
+While($Repeat) {
+  Write-Host "Starting script!"
+  $servers = Get-ADComputer -Filter {OperatingSystem -Like }
+}
+#jdbajof
+#test1
+for (){
+  
+}
