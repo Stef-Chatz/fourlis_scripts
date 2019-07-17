9d100e98e6e8a2b864e59ec8aa163561fc7d63a8
Stefanos Chatzakis <sxatzakis@yahoo.gr>
2019-07-17 13:44:40 +0300

    finished get cpu

--- info_retrieve.ps1
+++ info_retrieve.ps1
@@ -10,49 +10,20 @@ The purpose of this script is to access the active directory computers and
 retrieve information regarding the operating system as well as hardware parts.
 #>
 
-while($Repeat) {
-  Write-Host "Starting script!"
-  $servers = Get-ADComputer -Filter {OperatingSystem -Like "*Windows 7*" -or OperatingSystem -Like "*Windows 8*" -or OperatingSystem -Like "*Windows 8.1*" -or OperatingSystem -Like "*Windows 10*"} |FT Name
-  Write-Host "Computers found:" -ForegroundColor Yellow
 
-  Write-Host $servers
+Write-Host "Starting script!"
+$computers = Get-ADComputer -Filter {OperatingSystem -Like "*Windows 7*" -or OperatingSystem -Like "*Windows 8*" -or OperatingSystem -Like "*Windows 8.1*" -or OperatingSystem -Like "*Windows 10*"} |FT Name
+Write-Host "Computers found:" -ForegroundColor Yellow
 
-  foreach ($server in $servers) {
-    $Bios = Get-WmiObject win32_bios -Computername $Computer
-    $Hardware = Get-WmiObject Win32_computerSystem -Computername $Computer
-    $Sysbuild = Get-WmiObject Win32_WmiSetting -Computername $Computer
-    $OS = Get-WmiObject Win32_OperatingSystem -Computername $Computer
-    $Networks = Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $Computer | Where-Object {$_.IPEnabled}
-    $driveSpace = Get-WmiObject win32_volume -computername $Computer -Filter 'drivetype = 3' | 
-    Select-Object PScomputerName, driveletter, label, @{LABEL='GBfreespace';EXPRESSION={'{0:N2}' -f($_.freespace/1GB)} } |
-    Where-Object { $_.driveletter -match 'C:' }
-    $cpu = Get-WmiObject Win32_Processor  -computername $computer
-    $username = Get-ChildItem "\\$computer\c$\Users" | Sort-Object LastWriteTime -Descending | Select-Object Name, LastWriteTime -first 1
-    $totalMemory = [math]::round($Hardware.TotalPhysicalMemory/1024/1024/1024, 2)
-    $lastBoot = $OS.ConvertToDateTime($OS.LastBootUpTime) 
+Write-Host $computers
 
-    $IPAddress  = $Networks.IpAddress[0]
-    $MACAddress  = $Networks.MACAddress
-    $systemBios = $Bios.serialnumber
+foreach ($computer in $computers) {
 
-    $OutputObj  = New-Object -Type PSObject
-    $OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer.ToUpper()
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Manufacturer -Value $Hardware.Manufacturer
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Model -Value $Hardware.Model
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Processor_Type -Value $cpu.Name
-    $OutputObj | Add-Member -MemberType NoteProperty -Name System_Type -Value $Hardware.SystemType
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Operating_System -Value $OS.Caption
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Operating_System_Version -Value $OS.version
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Operating_System_BuildVersion -Value $SysBuild.BuildVersion
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Serial_Number -Value $systemBios
-    $OutputObj | Add-Member -MemberType NoteProperty -Name IP_Address -Value $IPAddress
-    $OutputObj | Add-Member -MemberType NoteProperty -Name MAC_Address -Value $MACAddress
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Last_User -Value $username.Name
-    $OutputObj | Add-Member -MemberType NoteProperty -Name User_Last_Login -Value $username.LastWriteTime
-    $OutputObj | Add-Member -MemberType NoteProperty -Name C:_FreeSpace_GB -Value $driveSpace.GBfreespace
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Total_Memory_GB -Value $totalMemory
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Last_ReBoot -Value $lastboot
-    $OutputObj | Export-Csv $exportLocation -Append -NoTypeInformation
-    }#end Invoke-Command
+  Write-Host "For computer:" $Computer -ForegroundColor Yellow
+  Write-Host "CPU:"
+  
+  $cpu = Get-WmiObject Win32_Processor  -computername $computer | Select name
+  
+  Write-Host $cpu
+
   }#end foreach
-}#end while
