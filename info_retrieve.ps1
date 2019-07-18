52c49aceaeb03f8c7cea350cdbf89df38714f714
Stefanos Chatzakis <sxatzakis@yahoo.gr>
2019-07-18 12:02:42 +0300

    added ram view

--- info_retrieve.ps1
+++ info_retrieve.ps1
@@ -12,15 +12,33 @@ retrieve information regarding the operating system as well as hardware parts.
 
 Write-Host "Starting script!"
 
-$memtype = @{
-  0 = 'Unknown'
-  1 = 'Other'
-  2 = 'DRAM'
-  20 = 'DDR'
-  21 = 'DDR-2'
-  22= 'DDR2 FB-DIMM'
-  24 = 'DDR3'
-  25 = 'FBD2'
+#Hash map for RAM type
+$MemoryTypeMap = @{
+  "0" = 'Unknown'
+  "1" = 'Other'
+  "2" = 'DRAM'
+  "3" = 'Synchronous DRAM'
+  "4" = 'Cache DRAM'
+  "5" = 'EDO'
+  "6" = 'EDRAM'
+  "7" = 'VRAM'
+  "8" = 'SRAM'
+  "9" = 'RAM'
+  "10" = 'ROM'
+  "11" = 'Flash'
+  "12" = 'EEPROM'
+  "13" = 'FEPROM'
+  "14" = 'EPROM'
+  "15" = 'CDRAM'
+  "16" = '3DRAM'
+  "17" = 'SDRAM'
+  "18" = 'SGRAM'
+  "19" = 'RDRAM'
+  "20" = 'DDR'
+  "21" = 'DDR2'
+  "22" = 'DDR2 FB-DIMM'
+  "24" = 'DDR3'
+  "25" = 'FBD2'
 }
 
 #prompt the admin for option.
@@ -43,21 +61,24 @@ if($ReadHost -eq "1") {
 
     Write-Host $cpu
 
-    #hard drive
-    $drives = Get-WmiObject win32_diskdrive | ?{$_.mediatype -eq "HDD" -or
-      $_.mediatype -eq "SSD"} | % -Process {$_.DeviceID} |Format-Table
-        Name, MediaType
+    #Hard drive
+    $drives = Get-WmiObject win32_diskdrive | Where-Object{$_.mediatype -eq "HDD" -or $_.mediatype -eq "SSD"} | ForEach-Object -Process {$_.DeviceID} |Format-Table Name, MediaType
 
     Write-Host "Hard Drives:"
     Write-Host $drives
 
+    #Memory
+    
+    $memory = Get-WmiObject Win32_PhysicalMemory |Select @{Label = 'Type';Expression = {$MemoryTypeMap["$($_.MemoryType)"]}}
+
+    Write-Host "Memory:"
+    Write-Host $memory
+  
   }#end if
 }#end if
 
 elseif ($Readhost -eq "2") {
-  $computers = Get-ADComputer -Filter {OperatingSystem -Like "*Windows 7*" -or
-    OperatingSystem -Like "*Windows 8*" -or OperatingSystem -Like
-      "*Windows 8.1*" -or OperatingSystem -Like "*Windows 10*"} |FT Name
+  $computers = Get-ADComputer -Filter {OperatingSystem -Like "*Windows 7*" -or OperatingSystem -Like "*Windows 8*" -or OperatingSystem -Like "*Windows 8.1*" -or OperatingSystem -Like "*Windows 10*"} |FT Name
   Write-Host "Computers found:" -ForegroundColor Yellow
 
   Write-Host $computers
@@ -72,7 +93,7 @@ elseif ($Readhost -eq "2") {
     }#end if
     Write-Host "For computer:" $Computer -ForegroundColor Yellow
     Write-Host "CPU:"
-    cpu = Get-WmiObject Win32_Processor  -computername $computer | Select name
+    cpu = Get-WmiObject Win32_Processor  -computername $computer | select name
 
     Write-Host $cpu
 
